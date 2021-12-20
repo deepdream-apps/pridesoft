e4261f9f0bb68f5d32f19562d6e3d936e73ee7d1
deepdream-apps <silvere.djam@gmail.com>
2021-12-20 10:10:53 +0100

    First commit for pridesoft-web

---
+++ pridesoft-web/src/test/java/cm/antic/pridesoft/web/PridesoftWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.antic.pridesoft.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PridesoftWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
